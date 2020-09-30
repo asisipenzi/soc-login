911a69622084ae0f1167006a9cfcd736b948f898
Andrew <sisipenzia@gmail.com>
2020-09-30 14:38:59 +0200

    Updated config.php

--- app/config.php
+++ app/config.php
@@ -2,15 +2,15 @@
 
 return [
     'database' => [
-        'dsn' => 'mysql:host=bojjpzxd2ycehnrypunn-mysql.services.clever-cloud.com;dbname=bojjpzxd2ycehnrypunn',
-        'username' => 'uoxxxlndwbgi5fef',
-        'password' => 'rQr4fad7HuCFZuBkaaHx'
+        'dsn' => 'mysql:host=localhost;dbname=fblogin',
+        'username' => 'root',
+        'password' => '309397as'
     ],
     'fb' => [
         'id' => '1210438962688158',
         'secret' => 'ff05707c23eb64fee634f237d2b5a089',
         'version' => 'v2.9',
             'permission' => ['email'],
-        'callback_url' => 'http://app-e26e8330-e894-462a-944e-0cbbfededfb7.cleverapps.io/fb-callback.php'
+        'callback_url' => 'http://localhost:8080/facebook/fb-callback.php'
     ]
 ];
